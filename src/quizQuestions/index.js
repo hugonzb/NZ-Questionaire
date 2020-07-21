34b6c3860586d34b4d7d8a5fa72443a5f365d475
Hugo Baird <hugonzb@gmail.com>
2020-07-21 20:05:15 +1200

    Added question bank

---
+++ src/quizQuestions/index.js
@@ -0,0 +1,305 @@
+const qBank = [
+    {
+      question: "What is Hugo's surname? Hint: He developed this quiz service",
+      answers: ["Bard", "Bezos", "Baird", "Kawamura"],
+      correct: "Baird",
+      questionId: "0001"
+    },
+    {
+      question: "How many stars does the New Zealand flag have?",
+      answers: ["4", "6", "None", "5"],
+      correct: "4",
+      questionId: "0002"
+    },
+    {
+      question: "New Zealand has three official languages. English and New Zealand sign language are two. What is the other one?",
+      answers: ["Japanese", "Maori", "Java", "French"],
+      correct: "Maori",
+      questionId: "0003"
+    },
+    {
+      question: "Aotearoa (New Zealand) means what?",
+      answers: ["Land of the long white cloud", "Land of the long green islands", "The legendary land", "Land of the big overseeing clouds"],
+      correct: "Land of the long white cloud",
+      questionId: "0004"
+    },
+    {
+      question: "Which New Zealand sports team has the best winning record of any male national team?",
+      answers: ["Silver Ferns", "All Blacks", "NZ Breakers", "Wellington Phoenix FC"],
+      correct: "All Blacks",
+      questionId: "0005"
+    },
+    {
+      question: "Which of these is NOT a popular New Zealand junk-food?",
+      answers: ["Twisties", "Cheetos", "Burger Rings", "Rashuns"],
+      correct: "Cheetos",
+      questionId: "0006"
+    },
+    {
+      question: "Which New Zealand film maker and screenwriter won an Oscar for Best Director in 2003?",
+      answers: ["Peter Jackson", "Peter Baird", "Taika Waititi", "Jane Campion"],
+      correct: "Peter Jackson",
+      questionId: "0007"
+    },
+    {
+      question: "What is the name of the largest city in the South Island",
+      answers: ["Invercargill", "Dunedin", "Christchurch", "Nelson"],
+      correct: "Christchurch",
+      questionId: "0008"
+    },
+    {
+      question: "Sir Edmund Hillary climbed Mount Everest in which year?",
+      answers: ["1999", "1940", "1959", "1953"],
+      correct: "1953",
+      questionId: "0009"
+    },
+    {
+      question: "Which of these fast-food chains is located in Wanaka?",
+      answers: ["McDonalds", "KFC", "Wendy's", "Domino's Pizza"],
+      correct: "Domino's Pizza",
+      questionId: "0010"
+    },
+    {
+      question: "When did NZ first reach a population of 1 million?",
+      answers: ["1940", "1908", "1922", "1890"],
+      correct: "1908",
+      questionId: "0011"
+    },
+    {
+      question: "Which of these New Zealand cities has NOT been the country's capital?",
+      answers: ["Queenstown", "Auckland", "Old Russell", "Wellington"],
+      correct: "Queenstown",
+      questionId: "0012"
+    },
+    {
+      question: "Which of these is NOT a top five country of origin for most immigrants to New Zealand (2001-2015)?",
+      answers: ["USA", "India", "UK", "China"],
+      correct: "USA",
+      questionId: "0013"
+    },
+    {
+      question: "Which of these is NOT a bird native to New Zealand?",
+      answers: ["Kea", "Kiwi", "Kookaburra", "Takahe"],
+      correct: "Kookaburra",
+      questionId: "0014"
+    },
+    {
+      question: "At which Olympics did Peter Snell win gold medals for the 800m and 1500m?",
+      answers: ["1964 Tokyo", "1960 Rome", "1968 Mexico City", "1956 Melbourne"],
+      correct: "1964 Tokyo",
+      questionId: "0015"
+    },
+    {
+      question: "What is the northernmost tip of New Zealand is called?",
+      answers: ["Auckland", "Northland", "Cape Reinga", "Bay of Islands"],
+      correct: "Cape Reinga",
+      questionId: "0016"
+    },
+    {
+      question: "Which of these animals can be found in the wild in New Zealand?",
+      answers: ["Snakes", "Bats", "Crocodiles", "Bears"],
+      correct: "Bats",
+      questionId: "0017"
+    },
+    {
+      question: "What is the main ingredient of the ANZAC biscuit?",
+      answers: ["Apples", "Rolled Oats", "Kumara", "Kiwi Fruit"],
+      correct: "Rolled Oats",
+      questionId: "0018"
+    },
+    {
+      question: "What’s the Kiwi word for the combination of fruits, nuts and chocolate trampers take with them?",
+      answers: ["Scroggin", "Trail Mix", "Yummy Mix", "Bangers"],
+      correct: "Scroggin",
+      questionId: "0019"
+    },
+    {
+      question: "What was the Kiwifruit called before it was renamed?",
+      answers: ["Japanese goji", "Greenfruit", "Feijoa berry", "Chinese gooseberry"],
+      correct: "Chinese gooseberry",
+      questionId: "0020"
+    },
+    {
+      question: "What is the population of New Zealand? (2018)",
+      answers: ["4.886 million", "4.557 million", "7.732 million", "13.372 million"],
+      correct: "4.886 million",
+      questionId: "0021"
+    },
+    {
+      question: "What is the population of Auckland? (2017)",
+      answers: ["3.223 million", "1.657 million", "900.232 thousand", "1.223 million"],
+      correct: "1.657 million",
+      questionId: "0022"
+    },
+    {
+      question: "What is the population of Wellington? (2017)",
+      answers: ["212.700 thousand","1.234 million","1.592 thousand","3.432 billion"],
+      correct: "212.700 thousand",
+      questionId: "0023"
+    },
+    {
+      question: "Which popular New Zealand comedian, who had their own show, died of a heart attack in 1991?",
+      answers: ["David McPhail", "Rhys Darby", "Jon Gadsby", "Billy T.James"],
+      correct: "Billy T.James",
+      questionId: "0024"
+    },
+    {
+      question: "Which small island off the southern coast of the South Island?",
+      answers: ["Easter Island", "Stewart Island", "Somes Island", "North Island"],
+      correct: "Stewart Island",
+      questionId: "0025"
+    },
+    {
+      question: "Which of these is a mountain located in Wanaka?",
+      answers: ["Mt. Steel", "Mt. Mauganui", "Mt. Iron", "Mt. Everest"],
+      correct: "Mt. Iron",
+      questionId: "0026"
+    },
+    {
+      question: "What was the capital of New Zealand before it was moved to Wellington?",
+      answers: ["Christchurch", "Taupo", "Dunedin", "Auckland"],
+      correct: "Auckland",
+      questionId: "0027"
+    },
+    {
+      question: "What is Wellington's harbour called?",
+      answers: ["Port Nicholson", "Wellington Harbour", "Port Eden", "Port Dixon"],
+      correct: "Port Nicholson",
+      questionId: "0028"
+    },
+    {
+      question: "What city is the Art Deco capital?",
+      answers: ["Taupo", "Nelson", "Napier", "Gisbourne"],
+      correct: "Napier",
+      questionId: "0029"
+    },
+    {
+      question: "What year was the Treaty of Waitangi was signed in?",
+      answers: ["1856", "1840", "1789", " 1809"],
+      correct: "1840",
+      questionId: "0030"
+    },
+    {
+      question: "New Zealand is part of a 93% submerged continent known as what?",
+      answers: ["Atlantis", "Zealandia", "Aotearoa", "Polynesia"],
+      correct: "Zealandia",
+      questionId: "0031"
+    },
+    {
+      question: "What is John Campbell well known as?",
+      answers: ["A circus clown", "An author", "A radio host", "A TV journalist"],
+      correct: "A TV journalist",
+      questionId: "0032"
+    },
+    {
+      question: "Former New Zealand Prime Minister Helen Clark is affiliated with which political party?",
+      answers: ["National", "Labor", "Act", "Greens"],
+      correct: "Labor",
+      questionId: "0033"
+    },
+    {
+      question: "What rank did New Zealand get in the safest countries in the world?",
+      answers: ["8", "1", "2", "6"],
+      correct: "2",
+      questionId: "0034"
+    },
+    {
+      question: "Approximately how many of New Zealand's native plant and animal species are deemed to be threatened or at risk of extinction?",
+      answers: ["400", "200", "3600", "4000"],
+      correct: "4000",
+      questionId: "0035"
+    },
+    {
+      question: "How many species of kiwi still exist?",
+      answers: ["1", "8", "5", "11"],
+      correct: "5",
+      questionId: "0036"
+    },
+    {
+      question: "What is the longest river in New Zealand?",
+      answers: ["Clutha River", "Waikato River", "Clarence River", "Rangitikei River"],
+      correct: "Waikato River",
+      questionId: "0037"
+    },
+    {
+      question: "What recreational activity was Jean Batten most famous for?",
+      answers: ["Dancing", "Singing", "Skydiving", "Flying"],
+      correct: "Flying",
+      questionId: "0038"
+    },
+    {
+      question: "What was Sir Edmund Hillary's original job in NZ",
+      answers: ["Programmer", "Accountant", "Beekeeper", "DJ"],
+      correct: "Beekeeper",
+      questionId: "0039"
+    },
+    {
+      question: "What is the highest volcano in New Zealand?",
+      answers: ["Mt. Ruapehu", "Mt. Tongariro", "Mt. Taranaki", "Mt. Cook"],
+      correct: "Mt. Ruapehu",
+      questionId: "0040"
+    },
+    {
+      question: "The large NZ insect which is often found in caves",
+      answers: ["House fly", "Mosquito", "Spider", "Weta"],
+      correct: "Weta",
+      questionId: "0041"
+    },
+    {
+      question: "What is the largest company in New Zealand?",
+      answers: ["ADInstruments", "Deloitte", "Xero", "Fonterra"],
+      correct: "Fonterra",
+      questionId: "0042"
+    },
+    {
+      question: "Who is on the New Zealand $100 note?",
+      answers: ["Brad Pitt", "Edmund Hillary", "Kate Sheppard", "Ernest Rutherford"],
+      correct: "Ernest Rutherford",
+      questionId: "0043"
+    },
+    {
+      question: "What is the deepest lake in New Zealand",
+      answers: ["Mariana Trench", "Lake Tekapo", "Lake Wanaka", "Lake Hauroko"],
+      correct: "Lake Hauroko",
+      questionId: "0044"
+    },
+    {
+      question: "What region is Wanaka part of?",
+      answers: ["Northland", "West Coast", "Otago", "Queensland"],
+      correct: "Otago",
+      questionId: "0045"
+    },
+    {
+      question: "What is Known as the 'Garden City'?",
+      answers: ["Dunedin", "Christchurch", "Invercargill", "Queenstown"],
+      correct: "Christchurch",
+      questionId: "0046"
+    },
+    {
+      question: "What location has a sculpture of a brown trout?",
+      answers: ["Gore", "Nelson", "Palmerston North", "Tekapo"],
+      correct: "Gore",
+      questionId: "0047"
+    },
+    {
+      question: "What is referred to as the 'Penguin Capital'",
+      answers: ["Greymouth", "New Plymouth", "Twizel", "Oamaru"],
+      correct: "Oamaru",
+      questionId: "0048"
+    },
+    {
+      question: "What is New Zealand's international dialling code?",
+      answers: ["+10", "+23", "+76", "+64"],
+      correct: "+64",
+      questionId: "0049"
+    },
+    {
+      question: "How long does it take to drive from Dunedin to Wanaka?",
+      answers: ["3 hours", "3 hours and 30 minutes", "4 hours", "10 minutes"],
+      correct: "3 hours and 30 minutes",
+      questionId: "0050"
+    }
+  ];
+  
+  export default (n = 5) =>
+    Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
