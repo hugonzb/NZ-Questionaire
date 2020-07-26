09ff9bab877f268fe4a6d6cb616a535779556ba3
Hugo Baird <hugonzb@gmail.com>
2020-07-26 16:55:09 +1200

    added state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
 
 class nz_questionaire extends Component {
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0
+  };
+  
   render(){
     return(
       "hello"
     )
   }
 }
-
 
 export default nz_questionaire;
