03d27f5636c5fa8d12ecb25c7cfcffdc9176936f
Hugo Baird <hugonzb@gmail.com>
2020-07-26 16:58:14 +1200

    Question Box component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import quizQuestions from "./quizQuestions";
 
 class nz_questionaire extends Component {
   state = {
     questionBank: [],
     score: 0,
     responses: 0
+  };
+
+  getQuestions = () => {
+    quizQuestions().then(question => {
+      this.setState({
+        questionBank: question
+      });
+    });
   };
   
   render(){
