37bb3c0a8a4f4d1058203c86d7b21f2502aa6abb
Hugo Baird <hugonzb@gmail.com>
2020-07-27 18:47:49 +1200

    Added compute answer

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ class nz_questionaire extends Component {
     });
   };
 
+  computeAnswer = (answer, correctAnswer) => {
+    if(answer===correctAnswer) {
+      this.setState({
+        score: this.state.score + 1
+      });
+   }
+   this.setState({
+     responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+   });
+  };
+
   componentDidMount() {
     this.getQuestions();
   }
@@ -30,11 +41,12 @@ class nz_questionaire extends Component {
         </div>
         <div className="questions">     
           {this.state.questionBank.map(
-              ({question, answers, questionId}) => (
+              ({question, answers, correct, questionId}) => (
                 <QuestionBox
                   question={question}
                   options={answers}
                   key={questionId}
+                  selected={answer => this.computeAnswer(answer, correct)}
                   />
               )  
             )}
