eb5039dcb1d8c372003c16084eb26a4b4ded4754
Hugo Baird <hugonzb@gmail.com>
2020-07-27 22:05:16 +1200

    Updates

---
+++ precache-manifest.0afba1d28406f881a7e09d0cb01b1573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db501a00cc6b00ae5739a7f18d9b0c27",
+    "url": "/NZ-General-Knowledge-Quiz/index.html"
+  },
+  {
+    "revision": "2b3233f19fe3eb23a22c",
+    "url": "/NZ-General-Knowledge-Quiz/static/css/main.a00179c2.chunk.css"
+  },
+  {
+    "revision": "f048ff2b2b3bf010400a",
+    "url": "/NZ-General-Knowledge-Quiz/static/js/2.392d1212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NZ-General-Knowledge-Quiz/static/js/2.392d1212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3233f19fe3eb23a22c",
+    "url": "/NZ-General-Knowledge-Quiz/static/js/main.5ba5e353.chunk.js"
+  },
+  {
+    "revision": "48befd1651ea92be53ba",
+    "url": "/NZ-General-Knowledge-Quiz/static/js/runtime-main.d010ddd6.js"
+  }
+]);
